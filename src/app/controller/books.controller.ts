4400330ec40a6fe023dbd25261190a07fce6aa80
nayeem-miah <nayeem5113a@gmail.com>
2025-07-13 16:44:17 +0600

    deleted a book by id

--- src/app/controller/books.controller.ts
+++ src/app/controller/books.controller.ts
@@ -74,6 +74,7 @@ bookRouter.get("/books/:bookId", async (req: Request, res: Response) => {
         })
     }
 });
+
 // update a book by id
 bookRouter.patch("/books/:bookId", async (req: Request, res: Response) => {
     try {
@@ -85,6 +86,35 @@ bookRouter.patch("/books/:bookId", async (req: Request, res: Response) => {
             message: "Book updated successfully",
             data: result
         })
+
+    } catch (error) {
+        console.error("something is wrong", error);
+        res.status(500).json({
+            success: false,
+            message: "something is wrong",
+            error: error
+        })
+    }
+});
+
+// deleted a book by id
+bookRouter.delete("/books/:bookId", async (req: Request, res: Response) => {
+    try {
+        const bookId = req.params.bookId
+
+        const result = await Book.findByIdAndDelete(bookId)
+        if (result) {
+            res.status(200).json({
+                success: true,
+                message: "Book updated successfully",
+                data: result
+            })
+        } else {
+            res.status(404).json({
+                success: false,
+                message: "Book not found",
+            })
+        }
 
     } catch (error) {
         console.error("something is wrong", error);
