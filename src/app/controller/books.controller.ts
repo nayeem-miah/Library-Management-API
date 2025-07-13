dc58ac6bc8b1873dcb323e38b8d9b5d831c28d1f
nayeem-miah <nayeem5113a@gmail.com>
2025-07-13 16:20:21 +0600

    book filtering successt

--- src/app/controller/books.controller.ts
+++ src/app/controller/books.controller.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import { Book } from '../modular/books.model';
 const bookRouter = express.Router();
 
-
+// post a book
 bookRouter.post("/books", async (req: Request, res: Response) => {
     try {
         const newBook = req.body;
@@ -20,5 +20,38 @@ bookRouter.post("/books", async (req: Request, res: Response) => {
             error: error
         })
     }
+});
+
+//  get all books
+bookRouter.get("/books", async (req: Request, res: Response) => {
+    try {
+        const filter = req.query.filter ? req.query.filter : "";
+        let result;
+        if (filter) {
+            result = await Book.find({ genre: filter });
+            res.status(200).json({
+                success: true,
+                message: "Books retrieved successfully",
+                data: result,
+            })
+
+        } else {
+            result = await Book.find();
+            res.status(200).json({
+                success: true,
+                message: "Books retrieved successfully",
+                data: result,
+            })
+        }
+
+    } catch (error) {
+        console.error("something is wrong", error);
+        res.status(500).json({
+            success: false,
+            message: "something is wrong",
+            error: error
+        })
+    }
 })
+
 export default bookRouter;
