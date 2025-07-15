4dff8cabe1fcbf9cb18773028c48a25675d28179
nayeem-miah <nayeem5113a@gmail.com>
2025-07-15 10:22:17 +0600

    tsc and vercel.json

---
+++ dist/app.js
@@ -0,0 +1,33 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const books_controller_1 = __importDefault(require("./app/controller/books.controller"));
+const borrow_controller_1 = require("./app/controller/borrow.controller");
+const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
+const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalErrorHandler"));
+exports.app = (0, express_1.default)();
+// Middleware to parse JSON bodies
+exports.app.use(express_1.default.json());
+dotenv_1.default.config();
+// application router
+exports.app.use("/api", books_controller_1.default);
+exports.app.use("/api", borrow_controller_1.borrowRouter);
+exports.app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.send("Hello server");
+}));
+exports.app.use(notFound_1.default);
+exports.app.use(globalErrorHandler_1.default);
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
+
 import { Server } from "http"
 import { app } from "./app";
 import mongoose from "mongoose";
 
 const port = process.env.PORT || 5000;
 const uri = process.env.MONGO_URI as string
+
 let server: Server;
 const main = async () => {
 
